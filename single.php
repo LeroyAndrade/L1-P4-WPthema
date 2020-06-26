9b237806d86486e1eab66689111caa02d1977124
LeroyAndrade <30168@ma-web.nl>
2020-06-26 23:31:10 +0200

    +tussentijdse update
    +tussentijdse update

---
+++ single.php
@@ -0,0 +1,41 @@
+<!--https://blanken5.home.xs4all.nl/WP_themaMaken.html
+https://developer.wordpress.org/themes/basics/main-stylesheet-style-css/
+opdr 4-->
+
+<?php get_header(); ?>
+<div class="container">
+  <div class="row">
+      <main class="col-md-9 container">
+        <div class="row">
+          <?php
+          if ( have_posts() ) : //if true
+              while( have_posts()) : the_post(); ?>
+
+                <article class="col-md-6">
+                  <p><?php the_title();?></p>
+                  <p><?php the_content();?></p>
+                  <div class="text-center p-4">
+                    <a href="<?php the_permalink();?>" class="btn btn-outline-primary">Lees meer ...</a>
+                  </div>
+                </article>
+              <?php
+              endwhile; else: ?>
+              <p>Geen berichten gevonden.</p>
+          <?php endif; ?>
+
+        </div>
+
+      </main>
+
+  <aside class="bg-light col-md-3">
+    <h3>
+      <div id="sidebar-primary" class="sidebar">
+        <?php dynamic_sidebar( 'aside' ); ?>
+      </div>
+
+
+    </h3>
+  </aside>
+</div>
+</div>
+    <?php get_footer(); ?>
