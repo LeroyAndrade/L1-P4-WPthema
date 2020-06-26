37edb8b740ee6bc1e5a3b7941da118278258cf51
LeroyAndrade <30168@ma-web.nl>
2020-06-27 00:49:29 +0200

    +Tussentijdse update
    +Tussentijdse update

---
+++ themaLe/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html <?php language_attributes();?> dir="ltr">
+  <head>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta name="author" content="Leroy Andrade">
+    <meta name="description" content="Wordpress, website,word press">
+    <meta name="keywords" content="Wordpress, website,word press">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php bloginfo("name"); ?></title>
+      <?php wp_head(); ?>
+
+<!-- dit zorgt voor meer ruimte zodat de admin bar goed wordt getoond midders wp_footer-->
+<!--
+https://developer.wordpress.org/themes/basics/including-css-javascript/
+-->
+  </head>
+
+  <body>
+    <header>
+
+<nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Wat Leroy zich mee bezich houd</a>
+        <?php
+        wp_nav_menu( array(
+            'theme_location'    => 'hoofd-menu',
+            'depth'             => 2,
+            'container'         => 'div',
+            'container_class'   => 'collapse navbar-collapse',
+            'container_id'      => 'bs-example-navbar-collapse-1',
+            'menu_class'        => 'nav navbar-nav',
+            'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+            'walker'            => new WP_Bootstrap_Navwalker(),
+        ) );
+        ?>
+        <a href="<?php echo home_url();?>">
+          <img style="width: 100px;"src="<?php echo get_template_directory_uri() . '/splash/logoL.png';?> " alt="Logo ">
+        </a>
+  </div>
+
+
+
+</nav>
+        <div class="jumbotron" style="background-repeat: no-repeat;background-image: url(<?php echo get_template_directory_uri() . '/splash/download.jpg' ?>);">
+          <div class="container">
+
+          <h1 class="display-4 p4"><a href="<?php echo home_url(); ?>"> <?php bloginfo("name"); ?></a></h1>
+          <p class="lead"><?php bloginfo('description');?></p>
+          </div>
+        </div>
+      </header>
---
+++ themaLe/index.php
@@ -0,0 +1,39 @@
+<!--https://blanken5.home.xs4all.nl/WP_themaMaken.html
+https://developer.wordpress.org/themes/basics/main-stylesheet-style-css/
+opdr 4-->
+
+<?php get_header(); ?>
+<div class="container">
+  <div class="row">
+      <main class="col-md-9 container">
+
+          <div class="row">
+            <?php
+            if ( have_posts() ) : //if true
+                while( have_posts()) : the_post(); ?>
+
+                  <div class="col-md-6">
+                    <h3><a href="<?php the_permalink();?>"><?php the_title();?></a></h3>
+                    <div><?php the_excerpt();?></div>
+                    <div class="text-center p-4">
+                      <a href="<?php the_permalink();?>" class="btn btn-outline-primary">Lees meer ...</a>
+                    </div>
+                  </div>
+
+                <?php
+                endwhile; else: ?>
+                <p>Geen berichten gevonden.</p>
+            <?php endif; ?>
+          </div>
+      </main>
+
+  <aside class="bg-light col-md-4 p-4">
+
+
+        <?php dynamic_sidebar( 'aside' ); ?>
+
+
+  </aside>
+</div>
+</div>
+    <?php get_footer(); ?>
